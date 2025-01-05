48be0b3dce91f764382804467ca588fa9d0780a3
tejasvi@appeneure <tejasviraj8686@gmail.com>
2025-01-05 22:26:33 +0530

    configure appwrite login aut

---
+++ lib/appwrite.ts
@@ -0,0 +1,54 @@
+import {Client, Avatars, Account, OAuthProvider} from 'react-native-appwrite';
+import * as Linking from 'expo-linking';
+import { openAuthSessionAsync } from "expo-web-browser";
+
+
+export const config =  {
+    Platform: 'com.ReState',
+    endpoint: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT,
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+}
+
+export const client = new Client();
+
+client
+    .setEndpoint(config.endpoint!)
+    .setProject(config.projectId!)
+    .setPlatform(config.Platform!);
+
+
+export const avatar = new Avatars(client)  
+export const account = new Account(client)
+
+
+export async function login() {
+    try {
+      const redirectUri = Linking.createURL("/");
+  
+      const response = await account.createOAuth2Token(
+        OAuthProvider.Google,
+        redirectUri
+      );
+      if (!response) throw new Error("Create OAuth2 token failed");
+  
+      const browserResult = await openAuthSessionAsync(
+        response.toString(),
+        redirectUri
+      );
+      if (browserResult.type !== "success")
+        throw new Error("Create OAuth2 token failed");
+  
+      const url = new URL(browserResult.url);
+      const secret = url.searchParams.get("secret")?.toString();
+      const userId = url.searchParams.get("userId")?.toString();
+      if (!secret || !userId) throw new Error("Create OAuth2 token failed");
+  
+      const session = await account.createSession(userId, secret);
+      if (!session) throw new Error("Failed to create session");
+  
+      return true;
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
+  }
