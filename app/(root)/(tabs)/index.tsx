264b992e20aac71d4323a2fd989ca5954bd18e9c
tejasvi <tejasviraj8686@gmail.com>
2025-01-19 14:18:09 +0530

    configure the appwrite db

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Image, TouchableOpacity } from "react-native";
+import { View, Text, Image, TouchableOpacity, FlatList } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import images from "@/constants/images";
@@ -6,57 +6,89 @@ import icons from "@/constants/icons";
 import Search from "@/components/search";
 import Filters from "@/components/Filter";
 import { Card, FeaturedCard } from "@/components/Card";
+import { router } from "expo-router";
+import { useGlobalContext } from "@/lib/global-provider";
 
 const Index = () => {
+  const { user } = useGlobalContext();
+  const handleCardPress = (id: string) => router.push(`/properties/${id}`);
   return (
     <SafeAreaView className="bg-white h-full ">
-      <View className="px-5 ">
-        <View className="flex flex-row items-center justify-between mt-5">
-          <View className="flex flex-row items-center">
-            <Image source={images.avatar} className="size-12 rounded-full" />
-            <View className="flex flex-col items-start ml-2 justify-center">
-              <Text className="text-xs font-rubik text-black-100">
-                Good Morning
-              </Text>
-              <Text className="text-base font-rubik-medium text-black-300 ">
-                Tejasvi
-              </Text>
+      <FlatList
+        data={properties}
+        renderItem={({ item }) => (
+          <Card item={item} onPress={() => handleCardPress(item.$id)} />
+        )}
+        keyExtractor={(item) => item.toString()}
+        numColumns={2}
+        contentContainerClassName="pb-32"
+        columnWrapperClassName="flex gap-5 px-5"
+        showsVerticalScrollIndicator={false}
+        ListHeaderComponent={
+          <View className="px-5 ">
+            <View className="flex flex-row items-center justify-between mt-5">
+              <View className="flex flex-row items-center">
+                <Image
+                  source={{ uri: user?.avatar }}
+                  className="size-12 rounded-full"
+                />
+                <View className="flex flex-col items-start ml-2 justify-center">
+                  <Text className="text-xs font-rubik text-black-100">
+                    Good Morning
+                  </Text>
+                  <Text className="text-base font-rubik-medium text-black-300 ">
+                  {user?.name}
+                  </Text>
+                </View>
+              </View>
+              <Image source={icons.bell} className="size-6" />
+            </View>
+
+            <Search />
+            <View className="my-5">
+              <View className="flex flex-row items-center justify-between">
+                <Text className="text-xl font-rubik-bold text-black-300 ">
+                  Featured
+                </Text>
+                <TouchableOpacity>
+                  <Text className="text-base font-rubik-bold text-primary-300">
+                    See All
+                  </Text>
+                </TouchableOpacity>
+              </View>
+              <FlatList
+                  data={latestProperties}
+                  renderItem={({ item }) => (
+                    <FeaturedCard
+                      item={item}
+                      onPress={() => handleCardPress(item.$id)}
+                    />
+                  )}
+                  bounces={false}
+                  keyExtractor={(item) => item.$id}
+                  horizontal
+                  showsHorizontalScrollIndicator={false}
+                  contentContainerClassName="flex gap-5 mt-5"
+                />
+            </View>
+
+            <View className="mt-5">
+              <View className="flex flex-row items-center justify-between">
+                <Text className="text-xl font-rubik-bold text-black-300">
+                  Our Recommendation
+                </Text>
+                <TouchableOpacity>
+                  <Text className="text-base font-rubik-bold text-primary-300">
+                    See all
+                  </Text>
+                </TouchableOpacity>
+              </View>
+
+              <Filters />
             </View>
           </View>
-          <Image source={icons.bell} className="size-6" />
-        </View>
-
-        <Search />
-        <View className="my-5">
-          <View className="flex flex-row items-center justify-between">
-            <Text className="text-xl font-rubik-bold text-black-300 ">
-              Featured
-            </Text>
-            <TouchableOpacity>
-              <Text className="text-base font-rubik-bold text-primary-300">
-                See All
-              </Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-        {/* <FeaturedCard />
-        <Card /> */}
-
-        <View className="mt-5">
-          <View className="flex flex-row items-center justify-between">
-            <Text className="text-xl font-rubik-bold text-black-300">
-              Our Recommendation
-            </Text>
-            <TouchableOpacity>
-              <Text className="text-base font-rubik-bold text-primary-300">
-                See all
-              </Text>
-            </TouchableOpacity>
-          </View>
-
-          <Filters />
-        </View>
-      </View>
+        }
+      />
     </SafeAreaView>
   );
 };
--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -16,14 +16,14 @@ export const config = {
   platform: "com.real.state",
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
   projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
-  // databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID,
-  // galleriesCollectionId:
-  //   process.env.EXPO_PUBLIC_APPWRITE_GALLERIES_COLLECTION_ID,
-  // reviewsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_REVIEWS_COLLECTION_ID,
-  // agentsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_AGENTS_COLLECTION_ID,
-  // propertiesCollectionId:
-  //   process.env.EXPO_PUBLIC_APPWRITE_PROPERTIES_COLLECTION_ID,
-  // bucketId: process.env.EXPO_PUBLIC_APPWRITE_BUCKET_ID,
+  databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID,
+  galleriesCollectionId:
+    process.env.EXPO_PUBLIC_APPWRITE_GALLERIES_COLLECTION_ID,
+  reviewsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_REVIEWS_COLLECTION_ID,
+  agentsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_AGENTS_COLLECTION_ID,
+  propertiesCollectionId:
+    process.env.EXPO_PUBLIC_APPWRITE_PROPERTIES_COLLECTION_ID,
+  //bucketId: process.env.EXPO_PUBLIC_APPWRITE_BUCKET_ID,
 };
 
 export const client = new Client();
@@ -34,8 +34,8 @@ client
 
 export const avatar = new Avatars(client);
 export const account = new Account(client);
-// export const databases = new Databases(client);
-// export const storage = new Storage(client);
+export const databases = new Databases(client);
+export const storage = new Storage(client);
 
 export async function login() {
   try {
